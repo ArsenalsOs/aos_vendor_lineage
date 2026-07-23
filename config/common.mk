9076af2cfbe4803d681feb9dfc9244ea10604e0c
Y-D-Lu <460999218@qq.com>
2026-07-23 17:14:30 +0800

    ArsenalsOS: include vendor/arsenals/arsenalsos boot jar
    Change-Id: I13f3887f487c2da76e00abaa16fdb31006038b99

--- config/common.mk
+++ config/common.mk
@@ -306,3 +306,6 @@ include vendor/lineage/config/version.mk
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 -include vendor/lineage/config/partner_gms.mk
+
+# ArsenalsOS: AOS system service (arsenalsos boot jar)
+include vendor/arsenals/arsenalsos/config/common.mk
